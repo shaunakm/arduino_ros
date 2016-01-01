9a832d1099cf139ed53b9377c7583876a16141e1
Shaunak Manurkar <shaunaksmanurkar@gmail.com>
2016-01-01 20:56:02 +0530

    Inverse Kinematics in joint_angles.cpp

--- src/joint_angle.cpp
+++ src/joint_angle.cpp
@@ -1,7 +1,20 @@
 #include "ros/ros.h"
 #include "arduino_ros/data.h"
+#include <cmath>
+
+#define PI 3.1415927
 
 using namespace std;
+
+const float shoulder_link = 100;
+const float elbow_link = 150;
+float elbow_theta;
+float shoulder_theta;
+
+float elbow_angle(double, double);
+float shoulder_angle(double, double);
+
+arduino_ros::data joint_angles;
 
 int main(int argc, char **argv)
 {
@@ -12,11 +25,13 @@ int main(int argc, char **argv)
 
 	while(ros::ok())
 	{
-		arduino_ros::data joint_angles;
-		cout<<"Enter the angles of the joint: ";
-		cin>>joint_angles.angles[0];
-		joint_angles.angles[1] = 0;
-		joint_angles.angles[2] = 0;
+		double pos_x, pos_y;
+		cout<<"Enter the x and y position in mm: ";
+		cin>>pos_x>>pos_y;
+
+		joint_angles.angles[0] = 90.0; 
+		joint_angles.angles[1] = shoulder_angle(pos_x, pos_y);
+		joint_angles.angles[2] = elbow_angle(pos_x, pos_y);
 
 		pub.publish(joint_angles);
 
@@ -25,4 +40,25 @@ int main(int argc, char **argv)
 	}
 
 	return 0;
+}
+
+float elbow_angle(double x, double y)
+{
+	elbow_theta = atan2((double)(sqrt(1-(pow((pow(x,2)+pow(y,2)-pow(shoulder_link,2)-pow(elbow_link,2))/(pow(x,2)+pow(y,2)),2)))),
+		(double)((pow(x,2)+pow(y,2)-pow(shoulder_link,2)-pow(elbow_link,2))/(pow(x,2)+pow(y,2))));
+	//cout<<(180*elbow_theta/3.14159)<<endl;
+
+	elbow_theta = 180 * elbow_theta / PI;
+
+	return elbow_theta;
+}
+
+float shoulder_angle(double x, double y)
+{
+	shoulder_theta = atan2(y,x) - atan2(elbow_link*sin(elbow_angle(x,y)),
+		(shoulder_link + elbow_link*cos(elbow_angle(x,y))));
+
+	shoulder_theta = 180 * shoulder_theta / PI;
+
+	return shoulder_theta;
 }
