2d384711b529a9d2b6c8b2c1d6a92c98ab4e3b30
Shaunak Manurkar <shaunaksmanurkar@gmail.com>
2016-04-08 19:03:36 +0530

    Changed formula for Inverse Kinematics

---
+++ firmware/sharp.cpp
@@ -0,0 +1,6 @@
+// ROSserial sharpIR sensor example.
+
+#include <Arduino.h>
+#include <ros.h>
+#include <ros/time.h>
+#include <sensor_msgs/Range.h>
--- src/joint_angle.cpp
+++ src/joint_angle.cpp
@@ -2,12 +2,12 @@
 #include "arduino_ros/data.h"
 #include <cmath>
 
-#define PI 3.1415927
+#define pi 3.14159265359
 
 using namespace std;
 
-const float shoulder_link = 100;
-const float elbow_link = 150;
+const float l1 = 100;
+const float l2 = 150;
 float elbow_theta;
 float shoulder_theta;
 float base_theta;
@@ -33,8 +33,8 @@ int main(int argc, char **argv)
 
 		//joint_angles.angles[0] = base_angle(pos_x, pos_y);
 		joint_angles.angles[0] = 90;
-		joint_angles.angles[1] = shoulder_angle(pos_y, pos_x);
-		joint_angles.angles[2] = elbow_angle(pos_y, pos_x);
+		joint_angles.angles[1] = shoulder_angle(pos_x, pos_y);
+		joint_angles.angles[2] = elbow_angle(pos_x, pos_y);
 
 		pub.publish(joint_angles);
 
@@ -47,21 +47,25 @@ int main(int argc, char **argv)
 
 float elbow_angle(double x, double y)
 {
-	elbow_theta = atan2((double)(sqrt(1-(pow((pow(x,2)+pow(y,2)-pow(shoulder_link,2)-pow(elbow_link,2))/(pow(x,2)+pow(y,2)),2)))),
-		(double)((pow(x,2)+pow(y,2)-pow(shoulder_link,2)-pow(elbow_link,2))/(pow(x,2)+pow(y,2))));
+	double c2 = (pow(x,2)+pow(y,2)-pow(l1,2)-pow(l2,2))/(2*l1*l2);
+
+	elbow_theta = atan2(sqrt(1-pow(c2,2)),c2);
 	//cout<<(180*elbow_theta/3.14159)<<endl;
 
-	elbow_theta = 180 * elbow_theta / PI;
+	elbow_theta = 180 * elbow_theta / pi;
 
 	return elbow_theta;
 }
 
 float shoulder_angle(double x, double y)
 {
-	shoulder_theta = atan2(y,x) - atan2(elbow_link*sin(elbow_angle(x,y)),
-		(shoulder_link + elbow_link*cos(elbow_angle(x,y))));
 
-	shoulder_theta = 180 * shoulder_theta / PI;
+	double k2 = l2 * sin(elbow_angle(x,y) * pi / 180);
+	double k1 = l1 + l2 * cos(elbow_angle(x,y) * pi / 180);
+
+	shoulder_theta = atan2(y,x) - atan2(k2, k1);
+
+	shoulder_theta = 180 * shoulder_theta / pi;
 
 	return shoulder_theta;
 }
@@ -70,7 +74,7 @@ float base_angle(double x, double y)
 {
 	base_theta = atan2(y,x);
 
-	base_theta = 180 * base_theta / PI;
+	base_theta = 180 * base_theta / pi;
 
 	return base_theta;
 }
