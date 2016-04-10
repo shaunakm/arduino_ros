a681624a8682e50d20847d06726479e8f85e2456
Shaunak Manurkar <shaunaksmanurkar@gmail.com>
2016-04-10 20:14:47 +0530

    Completed path for pick and place mech

--- src/arm_angles.cpp
+++ src/arm_angles.cpp
@@ -14,7 +14,7 @@ float base_theta;
 
 float elbow_angle(double, double);
 float shoulder_angle(double, double);
-float base_angle(double, double);
+void path_planning(int);
 
 arduino_ros::data joint_angles;
 
@@ -23,24 +23,34 @@ int main(int argc, char **argv)
 	ros::init(argc,argv,"joint_angle");
 	ros::NodeHandle n;
 	ros::Publisher pub = n.advertise<arduino_ros::data>("angles",10000);
+	
 	ros::Rate loop_rate(10);
 
 	while(ros::ok())
 	{
 
-		int status = 0,object_status = 0;
+		int status = 1,object_status = 0;
 		bool isObject = false;
 
-		cout<<"Is object present?(1/0)";
+		cout<<"Initial Position"<<endl;
+		joint_angles.angles[0] = 90;
+		joint_angles.angles[1] = shoulder_angle(-68.3324, 197.322);
+		joint_angles.angles[2] = elbow_angle(-68.3324, 197.322);
+
+		pub.publish(joint_angles);
+
+		cout<<"Type of object(1/2): ";
 		cin>>object_status;
 
-		if(object_status == 1)
+		if(object_status != 0)
 			isObject = true;
 		else
 			isObject = false;
+
 		
 		if(isObject)
 		{
+			/*path_planning(status);
 			if(status == 0)
 			{
 				cout<<"Initial Position"<<endl;
@@ -50,8 +60,10 @@ int main(int argc, char **argv)
 
 				pub.publish(joint_angles);
 
+				ros::Duration(4).sleep();
+
 				status = 1;
-			}
+			}*/
 
 			if(status == 1)
 			{
@@ -61,6 +73,8 @@ int main(int argc, char **argv)
 				joint_angles.angles[2] = elbow_angle(-164.926, -78.5841);
 
 				pub.publish(joint_angles);
+
+				ros::Duration(4).sleep();
 
 				status = 2;
 			}
@@ -74,19 +88,38 @@ int main(int argc, char **argv)
 
 				pub.publish(joint_angles);
 
+				ros::Duration(4).sleep();
+
 				status = 3;
 			}
 
 			if(status == 3)
 			{
 				cout<<"Placing The Object"<<endl;
-				joint_angles.angles[0] = 45;
+
+				if(object_status == 1)
+					joint_angles.angles[0] = 45;
+				if(object_status == 2)
+					joint_angles.angles[0] = 135;
+				joint_angles.angles[1] = shoulder_angle(-68.3324, 197.322);
+				joint_angles.angles[2] = elbow_angle(-68.3324, 197.322);
+
+				pub.publish(joint_angles);
+
+				ros::Duration(2).sleep();
+
+				if(object_status == 1)
+					joint_angles.angles[0] = 45;
+				if(object_status == 2)
+					joint_angles.angles[0] = 135;
 				joint_angles.angles[1] = 360 + shoulder_angle(-164.926, -78.5841);
 				joint_angles.angles[2] = elbow_angle(-164.926, -78.5841);
 
 				pub.publish(joint_angles);
 
-				status = 0;
+				ros::Duration(4).sleep();
+
+				status = 1;
 			}
 			isObject = false;
 		}
@@ -120,4 +153,10 @@ float shoulder_angle(double x, double y)
 	shoulder_theta = 180 * shoulder_theta / pi;
 
 	return shoulder_theta;
+}
+
+
+void path_planning(int status)
+{
+	
 }
